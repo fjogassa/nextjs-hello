f8a2f8365ef6c5bcc4f6907ed0a7edb565577b91
Fabio Jun <fjogassa@gmail.com>
2026-02-19 22:05:02 -0300

    Criando o projeto Hello World

---
+++ app/api/hello/route.js
@@ -0,0 +1,11 @@
+export async function GET() {
+  return new Response(
+    JSON.stringify({ message: "Hello World" }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
